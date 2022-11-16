f10793ea9227465eec5d93f1dc5620bc083749ab
Steve Wyckoff <stevewyckoffdev@gmail.com>
2022-11-16 15:03:47 -0800

    feat(newNeedCode): automated renaming of the function and the types file

--- src/newNeetcode.js
+++ src/newNeetcode.js
@@ -1,5 +1,5 @@
 'use strict';
-import { copyFile } from 'fs';
+import { copyFile, readFileSync, writeFileSync } from 'fs';
 import * as readline from 'node:readline';
 import { dirname } from 'path';
 import { exit } from 'process';
@@ -33,6 +33,7 @@ rl.question(
 
 function main(filename) {
   createNeetcodeFiles(filename);
+  replaceFunctionName(filename);
 }
 
 function onError(err) {
@@ -54,4 +55,17 @@ function createNeetcodeFiles(filename) {
   copyNeedcodeFile('templateQuokkaFree.ts', `${filename}.ts`);
   copyNeedcodeFile('input/templateQuestions.json', `input/${filename}.json`);
   copyNeedcodeFile('types/templateTypes.ts', `types/${filename}.ts`);
+}
+
+function replaceFunctionName(filename) {
+  const filePath = `${__dirname}/neetcode/solutions/${filename}.ts`;
+  const filenameWithoutNumber = filename.split('_')[0];
+
+  let contents = readFileSync(filePath, 'utf-8');
+  const replacedFunctionName = contents.replace(
+    /replace/gi,
+    filenameWithoutNumber
+  );
+  const replacedTypes = replacedFunctionName.replace(/templateTypes/, filename);
+  writeFileSync(filePath, replacedTypes);
 }
